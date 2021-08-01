8fdffa43694f290a984b6807de28c6a782c92c68
T.J. Kolberg <tkolberg@asu.edu>
2021-08-01 16:25:35 -0600

    🔬  Experimental Node.JS Backend Option

--- scrape.js
+++ scrape.js
@@ -1,24 +1,24 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const fs = require('fs');
-const writeStream = fs.createWriteStream('post.csv');
+// const request = require('request');
+// const cheerio = require('cheerio');
+// const fs = require('fs');
+// const writeStream = fs.createWriteStream('post.csv');
 
-// Write Headers
-writeStream.write(`Title,Link,Date \n`);
+// // Write Headers
+// writeStream.write(`Title,Link,Date \n`);
 
-request('https://www.worldometers.info/coronavirus/', (error, response, html) => {
-  if (!error && response.statusCode == 200) {
-    const $ = cheerio.load(html);
+// request('https://www.worldometers.info/coronavirus/', (error, response, html) => {
+//   if (!error && response.statusCode == 200) {
+//     const $ = cheerio.load(html);
 
-    $('.post-main_table_countries_div').each((i, el) => {
-      const title = $(el)
+//     $('.post-main_table_countries_div').each((i, el) => {
+//       const title = $(el)
 
-      console.log(title)
+//       console.log(title)
 
-      // Write Row To CSV
-      writeStream.write(`${title}, ${link}, ${date} \n`);
-    });
+//       // Write Row To CSV
+//       writeStream.write(`${title}, ${link}, ${date} \n`);
+//     });
 
-    // console.log('Scraping Done...');
-  }
-});
+//     // console.log('Scraping Done...');
+//   }
+// });
